c4a37ad65d4ee3158e01ad422b770ae9f0e83f1d
Dominik Chmiel <dominik.chmiel@aiconomix.com>
2021-08-11 15:49:33 +0200

    Add repro for sentry issue

--- source/ContentScript/index.ts
+++ source/ContentScript/index.ts
@@ -1,3 +1,12 @@
-console.log('helloworld from content script');
+import * as Sentry from "@sentry/browser";
+
+try {
+	Sentry.init({
+	    dsn: "http://somednskey@sentry_onpremise_nginx_1:9000/101",
+	    release: "something",
+	});
+} catch (e) {
+	console.log(e)
+}
 
 export {};
